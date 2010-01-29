f8e6c15e78bb1a6d2904654aa1d53741ccb6bcdb
Benjamin "balupton" Lupton <contact@balupton.com>
2010-01-29 10:55:40 +0000

    * Doctrine Auth Adapter improvements.

--- lib/Bal/Auth/Adapter/Doctrine.php
+++ lib/Bal/Auth/Adapter/Doctrine.php
@@ -2,12 +2,17 @@
 require_once 'Zend/Auth/Adapter/Interface.php';
 class Bal_Auth_Adapter_Doctrine implements Zend_Auth_Adapter_Interface {
 	
+	const FAILURE_IDENTITY_NOT_FOUND_MESSAGE = 'The identity could not be found';
+	const FAILURE_CREDENTIAL_INVALID_MESSAGE = 'The credentials that were supplied are invalid';
+	const FAILURE_IDENTITY_INACTIVE_MESSAGE = 'The identity you chose is currently inactive';
+	
 	private $_username;
 	private $_password;
 	protected $_options = array(
 		'tableName' => 'User',
-		'indentityColumn' => 'username',
-		'credentialColumn' => 'password'
+		'identityColumn' => 'username',
+		'credentialColumn' => 'password',
+		'activeColumn' => false
 	);
 	
 	
@@ -41,17 +46,22 @@ class Bal_Auth_Adapter_Doctrine implements Zend_Auth_Adapter_Interface {
 	}
 	
 	# -----------
+	# Options
 	
 	public function setTableName ($value) {
 		$this->setOption('tableName', $value);
 		return $this;
 	}
 	public function setIdentityColumn ($value) {
-		$this->setOption('indentityColumn', $value);
+		$this->setOption('identityColumn', $value);
 		return $this;
 	}
 	public function setCredentialColumn ($value) {
 		$this->setOption('credentialColumn', $value);
+		return $this;
+	}
+	public function setActiveColumn ($value) {
+		$this->setOption('activeColumn', $value);
 		return $this;
 	}
 	
@@ -65,13 +75,25 @@ class Bal_Auth_Adapter_Doctrine implements Zend_Auth_Adapter_Interface {
      * @return Zend_Auth_Result
      */
     public function authenticate ( ) {
+		# Prepare
     	$Result = null;
-
+		
+		# Prepare Options
+		$tableName = $this->getOption('tableName');
+		$identityColumn = $this->getOption('identityColumn');
+		$credentialColumn = $this->getOption('credentialColumn');
+		$activeColumn = $this->getOption('activeColumn');
+		
+		# Prepre Credentials
+		$username = $this->_username;
+		$password = $this->_password;
+		
+		# Attempt
     	try {
     		# Prepare Query
 			$Query = Doctrine_Query::create()
-			    ->from($this->getOption('tableName').' u')
-			    ->where('u.'.$this->getOption('indentityColumn').' = ?', $this->_username);
+			    ->from($tableName.' u')
+			    ->where('u.'.$identityColumn.' = ?', $username);
 			
 			# Fetch User
 			$User = $Query->fetchOne();
@@ -82,27 +104,41 @@ class Bal_Auth_Adapter_Doctrine implements Zend_Auth_Adapter_Interface {
 				$Result = new Zend_Auth_Result(
 		            Zend_Auth_Result::FAILURE_IDENTITY_NOT_FOUND,
 		            null,
-		            array('The indentity could not be found')
+		            array(self::FAILURE_IDENTITY_NOT_FOUND_MESSAGE)
 				);
 			}
 			else {
-				if ( $User->get($this->getOption('credentialColumn')) !== $this->_password) {
-					# Invalid password
+				# Check Credentials
+				$credentialsMatch = false;
+				if ( method_exists($tableName, 'compareCredentials') )
+					$credentialsMatch = $User->compareCredentials($username,$password);
+				elseif ( $credentialColumn )
+					$credentialsMatch = $User->get($credentialColumn) !== $password;
+				
+				# Check Credentials
+				if ( !$credentialsMatch ) {
+					# Invalid Credentials
 					$Result = new Zend_Auth_Result(
 			            Zend_Auth_Result::FAILURE_CREDENTIAL_INVALID,
 			            $User->id,
-			            array('The credentials that were supplied are invalid')
+			            array(self::FAILURE_CREDENTIAL_INVALID_MESSAGE)
 					);
 				}
 				else {
+					# Check Activate
+					$isActive = false;
+					if ( method_exists($tableName, 'isActive') )
+						$isActive = $User->isActive();
+					elseif ( $activeColumn )
+						$isActive = $User->get($activeColumn) ? true : false;
+				
 					# Check Enabled
-					$Table = Doctrine::getTable($this->getOption('tableName'));
-					if ( $Table->hasField('enabled') && !$User->enabled ) {
+					if ( !$isActive ) {
 						# Account Disabled
 						$Result = new Zend_Auth_Result(
 				            Zend_Auth_Result::FAILURE_CREDENTIAL_INVALID,
 				            $User->id,
-				            array('The account is currently disabled')
+				            array(self::FAILURE_IDENTITY_INACTIVE_MESSAGE)
 						);
 					}
 					else {
